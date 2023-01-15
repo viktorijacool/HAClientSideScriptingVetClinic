2e1cf491f1f9257bc4d291a3f73b5a4270c2a022
Viktorija K <viktoria.kuluk@inbox.lv>
2023-01-15 22:01:31 +0200

    BusinessLogic almost completed View is needed to be added

--- HAEnterpriseProgramming/AsynchronousFileTransferWebsite/Controllers/FileController.cs
+++ HAEnterpriseProgramming/AsynchronousFileTransferWebsite/Controllers/FileController.cs
@@ -1,128 +1,128 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Hosting;
-//using BusinessLogic.Services;
-//using Domain.Models;
-//using Microsoft.AspNetCore.Http;
-//using System.Collections.Generic;
-//using System.IO;
-//using BusinessLogic.ViewModels;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using BusinessLogic.Services;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.IO;
+using BusinessLogic.ViewModels;
 
-//namespace AsynchronousFileTransferWebsite.Controllers
-//{
-//    public class FileController : Controller
-//    {
-//        private FileService fileService;
+namespace AsynchronousFileTransferWebsite.Controllers
+{
+    public class FileController : Controller
+    {
+        private FileService fileService;
 
-//        public FileController(FileService _fileService)
-//        {
-//            fileService = _fileService;
-//        }
-
-
-
-//        //a method to open the page, then the user starts typing
-//        public IActionResult Create()
-//        {
-//            return View();
-//        }
-
-//        //a method to handle the submission of the form 
-//        [HttpPost]
-//        public IActionResult Create(CreateTextFileViewModel data)
-//        {
-//            fileService.Create(data);    //to test
-
-//            return View();
-//        }
+        public FileController(FileService _fileService)
+        {
+            fileService = _fileService;
+        }
 
 
 
-//        //private readonly FileService _fileService;
-//        //private readonly IWebHostEnvironment _environment;
+        //a method to open the page, then the user starts typing
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-//        //public FileController(FileService fileService, IWebHostEnvironment environment)
-//        //{
-//        //    _fileService = fileService;
-//        //    _environment = environment;
-//        //}
+        //a method to handle the submission of the form 
+        [HttpPost]
+        public IActionResult Create(CreateTextFileViewModel data)
+        {
+            fileService.Create(data);    //to test
 
-//        //public IActionResult Share(int fileId)
-//        //{
-//        //    // Get the list of users who have access to the file
-//        //    var file = _fileService.GetFile(fileId);
-//        //    var recipients = file.Recipients;
+            return View();
+        }
 
-//        //    // Get the list of all users
-//        //    var allUsers = GetAllUsers();
 
-//        //    // Remove the recipients from the list of all users
-//        //    allUsers.RemoveAll(r => recipients.Contains(r));
 
-//        //    // Pass the list of users to the view
-//        //    ViewBag.Users = allUsers;
+        //private readonly FileService _fileService;
+        //private readonly IWebHostEnvironment _environment;
 
-//        //    return View();
-//        //}
+        //public FileController(FileService fileService, IWebHostEnvironment environment)
+        //{
+        //    _fileService = fileService;
+        //    _environment = environment;
+        //}
 
-//        //[HttpPost]
-//        //public IActionResult Share(int fileId, string recipient)
-//        //{
-//        //    // Share the file with the selected user
-//        //    _fileService.Share(fileId, recipient);
+        //public IActionResult Share(int fileId)
+        //{
+        //    // Get the list of users who have access to the file
+        //    var file = _fileService.GetFile(fileId);
+        //    var recipients = file.Recipients;
 
-//        //    return RedirectToAction("Index");
-//        //}
+        //    // Get the list of all users
+        //    var allUsers = GetAllUsers();
 
-//        //public IActionResult Create()
-//        //{
-//        //    return View();
-//        //}
+        //    // Remove the recipients from the list of all users
+        //    allUsers.RemoveAll(r => recipients.Contains(r));
 
-//        //[HttpPost]
-//        //public IActionResult Create(TextFileModel file, IFormFile data)
-//        //{
-//        //    // Save the file data to the Data folder
-//        //    var filePath = Path.Combine(_environment.WebRootPath, "Data", file.FileName);
-//        //    using (var stream = new FileStream(filePath, FileMode.Create))
-//        //    {
-//        //        data.CopyTo(stream);
-//        //    }
+        //    // Pass the list of users to the view
+        //    ViewBag.Users = allUsers;
 
-//        //    // Set the file data on the model
-//        //    file.Data = System.IO.File.ReadAllText(filePath);
+        //    return View();
+        //}
 
-//        //    // Create the file in the repository
-//        //    _fileService.Create(file);
+        //[HttpPost]
+        //public IActionResult Share(int fileId, string recipient)
+        //{
+        //    // Share the file with the selected user
+        //    _fileService.Share(fileId, recipient);
 
-//        //    return RedirectToAction("Index");
-//        //}
+        //    return RedirectToAction("Index");
+        //}
 
-//        //public IActionResult Edit(int fileId)
-//        //{
-//        //    // Validate that the user has permission to edit the file
-//        //    var file = _fileService.GetFile(fileId);
-//        //    if (file.Author != User.Identity.Name && !file.Recipients.Contains(User.Identity.Name))
-//        //    {
-//        //        return Unauthorized();
-//        //    }
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
 
-//        //    return View(file);
-//        //}
+        //[HttpPost]
+        //public IActionResult Create(TextFileModel file, IFormFile data)
+        //{
+        //    // Save the file data to the Data folder
+        //    var filePath = Path.Combine(_environment.WebRootPath, "Data", file.FileName);
+        //    using (var stream = new FileStream(filePath, FileMode.Create))
+        //    {
+        //        data.CopyTo(stream);
+        //    }
 
-//        //[HttpPost]
-//        //public IActionResult Edit(TextFileModel file)
-//        //{
-//        //    // Update the file in the repository
-//        //    _fileService.Edit(file.FileId, file.Data);
+        //    // Set the file data on the model
+        //    file.Data = System.IO.File.ReadAllText(filePath);
 
-//        //    return RedirectToAction("Index");
-//        //}
+        //    // Create the file in the repository
+        //    _fileService.Create(file);
 
-//        //private List<string> GetAllUsers()
-//        //{
-//        //    // Get the list of all users from the database or some other source
-//        //    // (This is just an example. You would need to implement this method for your application.)
-//        //    return new List<string> { "user1", "user2", "user3" };
-//        //}
-//    }
-//}
+        //    return RedirectToAction("Index");
+        //}
+
+        //public IActionResult Edit(int fileId)
+        //{
+        //    // Validate that the user has permission to edit the file
+        //    var file = _fileService.GetFile(fileId);
+        //    if (file.Author != User.Identity.Name && !file.Recipients.Contains(User.Identity.Name))
+        //    {
+        //        return Unauthorized();
+        //    }
+
+        //    return View(file);
+        //}
+
+        //[HttpPost]
+        //public IActionResult Edit(TextFileModel file)
+        //{
+        //    // Update the file in the repository
+        //    _fileService.Edit(file.FileId, file.Data);
+
+        //    return RedirectToAction("Index");
+        //}
+
+        //private List<string> GetAllUsers()
+        //{
+        //    // Get the list of all users from the database or some other source
+        //    // (This is just an example. You would need to implement this method for your application.)
+        //    return new List<string> { "user1", "user2", "user3" };
+        //}
+    }
+}
--- HAEnterpriseProgramming/BusinessLogic/Services/FileService.cs
+++ HAEnterpriseProgramming/BusinessLogic/Services/FileService.cs
@@ -1,93 +1,93 @@
-﻿//using BusinessLogic.ViewModels;
-//using Domain.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
+﻿using BusinessLogic.ViewModels;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-//namespace BusinessLogic.Services
-//{
-//    public class FileService
-//    {
-//        public FileService() { }
+namespace BusinessLogic.Services
+{
+    public class FileService
+    {
+        public FileService() { }
 
-//        private TextFileDbRepository txtfr;
+        private TextFileDbRepository txtfr;
 
-//        public FileService(TextFileDbRepository _textFileRepository)
-//        {
-//            txtfr = _textFileRepository;
-//        }
+        public FileService(TextFileDbRepository _textFileFepository)
+        {
+            txtfr = _textFileFepository;
+        }
 
-//        public void Create(CreateTextFileViewModel textFile)
-//        {
+        public void Create(CreateTextFileViewModel textFile)
+        {
 
-//            if (txtfr.GetFileEntries().Any(f => f.FileName == textFile.FileName))
-//                throw new Exception("File with the same name already exists");
-//            else
-//            {
-//                txtfr.Create(new Domain.Models.TextFileModel()
-//                {
-//                    FileName = textFile.FileName,
-//                    UploadedOn = textFile.UploadedOn,
-//                    Data = textFile.Data,
-//                    Author = textFile.Author,
-//                    LastEditedBy = textFile.LastEditedBy,
-//                    LastUpdated = textFile.LastUpdated
-//                });
-//            }
+            if (txtfr.GetFileEntries().Any(f => f.FileName == textFile.FileName))
+                throw new Exception("File with the same name already exists");
+            else
+            {
+                txtfr.Create(new Domain.Models.TextFileModel()
+                {
+                    FileName = textFile.FileName,
+                    UploadedOn = textFile.UploadedOn,
+                    Data = textFile.Data,
+                    Author = textFile.Author,
+                    LastEditedBy = textFile.LastEditedBy,
+                    LastUpdated = textFile.LastUpdated
+                });
+            }
 
-//        }
-
-
-//        //public void Create(TextFileModel file)
-//        //{
-//        //    // Calculate the checksum of the file data
-//        //    var checksum = CalculateChecksum(file.Data);
-
-//        //    // Set the checksum on the file model
-//        //    file.Checksum = checksum;
-
-//        //    // Create the file in the repository
-//        //    _textFileRepository.Create(file);
-//        //}
+        }
 
 
-//        //public void Share(int fileId, string recipient)
-//        //{
-//        //    txtfr.Share(fileId, recipient);
-//        //}
+        public void Create(TextFileModel file)
+        {
+            // calculate the checksum of the file data
+            var checksum = CalculateChecksum(file.data);
 
-//        //public void Edit(int fileId, string changes)
-//        //{
-//        //    // Validate that the user has permission to edit the file
-//        //    var file = txtfr.GetFile(fileId);
-//        //    if (file.Author != User.Identity.Name && !file.Recipients.Contains(User.Identity.Name))
-//        //    {
-//        //        throw new UnauthorizedAccessException("You do not have permission to edit this file.");
-//        //    }
+            // set the checksum on the file model
+            file.checksum = checksum;
 
-//        //    // Calculate the checksum of the updated data
-//        //    var checksum = CalculateChecksum(changes);
+            // create the file in the repository
+            _textFileFepository.create(file);
+        }
 
-//        //    // Update the file in the repository
-//        //    txtfr.Edit(fileId, changes, checksum);
-//        //}
 
-//        //public TextFileModel GetFile(int fileId)
-//        //{
-//        //    return txtfr.GetFile(fileId);
-//        //}
+        public void Share(int fileid, string recipient)
+        {
+            txtfr.Share(fileid, recipient);
+        }
 
-//        //public List<TextFileModel> GetPermissions()
-//        //{
-//        //    return txtfr.GetPermissions();
-//        //}
+        public void Edit(int fileid, string changes)
+        {
+            // validate that the user has permission to edit the file
+            var file = txtfr.GetFile(fileId);
+            if (file.author != user.identity.name && !file.recipients.contains(user.identity.name))
+            {
+                throw new UnauthorizedAccessException("You do not have permission to edit this file.");
+            }
 
-//        //private string CalculateChecksum(string data)
-//        //{
-//        //    // Calculate the checksum of the data
-//        //    // (This is just an example. You should use a more secure hash algorithm in production.)
-//        //    return data.GetHashCode().ToString();
-//        //}
-//    }
-//}
+            // calculate the checksum of the updated data
+            var checksum = CalculateChecksum(changes);
+
+            // update the file in the repository
+            txtfr.Edit(fileid, changes, checksum);
+        }
+
+        public TextFileModel GetFile(int fileId)
+        {
+            return txtfr.GetFile(fileId);
+        }
+
+        public List<TextFileModel> GetPermissions()
+        {
+            return txtfr.GetPermissions();
+        }
+
+        private string CalculateChecksum(string data)
+        {
+            // calculate the checksum of the data
+            // (this is just an example. you should use a more secure hash algorithm in production.)
+            return data.GetHashCode().ToString();
+        }
+    }
+}
